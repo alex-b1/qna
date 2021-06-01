acaa1490ac4627e17b7f145f1e011e86f500ea4d
Alex Bogomolov <a.bogomolov@balance-pl.ru>
2021-06-01 23:51:59 +0000

    add sign_out_spec sign_up_spec

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  In order to close session
+  As an authenticated user
+  I'd like to be able to sign out
+} do
+
+  given(:user) { create(:user) }
+
+  background { visit new_user_session_path }
+
+  scenario 'Authenticated user can sign out' do
+    sign_in(user)
+    click_on 'Log out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
