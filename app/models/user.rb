380a1d3c9663aa1f29edd164ff65d82d389d765d
Alex Bogomolov <a.bogomolov@balance-pl.ru>
2021-06-29 16:34:51 +0000

    feature/part12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,31 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable,
+         :omniauthable,
+         omniauth_providers: [:github, :vkontakte]
 
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :rewards, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
 
   def author?(item)
     id == item.user_id
+  end
+
+  def self.find_for_oauth(auth)
+    FindForOauthService.new(auth).call
+  end
+
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
