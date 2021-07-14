3312ed80e4cd9c34cabb5833a3dae97c8efb269d
Alex Bogomolov <a.bogomolov@balance-pl.ru>
2021-07-14 20:30:28 +0000

    babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -42,12 +42,11 @@ module.exports = function(api) {
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
-        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-private-methods',
         {
           loose: true
         }
       ],
-      ['@babel/plugin-proposal-private-methods', { loose: true }],
       [
         '@babel/plugin-proposal-object-rest-spread',
         {
