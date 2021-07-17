15e5a9169b4ebaf6ba0769c624d14b8455b9473f
Alex Bogomolov <a.bogomolov@balance-pl.ru>
2021-07-17 14:02:34 +0000

    feature/part20

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Qna
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
+    
     config.active_job.queue_adapter = :sidekiq
 
     config.generators do |g|
